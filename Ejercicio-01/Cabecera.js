ffffddfa5c7f7c0413f1440d0df686f416f29296
UO258273 <71766164+UO258273@users.noreply.github.com>
2020-11-18 14:04:50 +0100

    Primer intento ejercicio 1 de ECMAScript

---
+++ Ejercicio-01/Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-21";
+asignatura.estudiante = "Luis Martinez Moreno";
+asignatura.email = "UO258273@uniovi.es";
