1cf4cedcc6e2625e5b92ac9f185bf3d5f558cce9
UO258273 <71766164+UO258273@users.noreply.github.com>
2020-11-19 22:10:34 +0100

    Tarea6Ejercicio11

---
+++ Tarea-6/Ejercicio11.js
@@ -0,0 +1,49 @@
+function initMap() {
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  navigator.geolocation.getCurrentPosition(this.mostrar);
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: { lat: 43.361747, lng: -5.849877 },
+  });
+  directionsRenderer.setMap(map);
+  directionsRenderer.setPanel(document.getElementById("right-panel"));
+  const control = document.getElementById("floating-panel");
+  control.style.display = "block";
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+  const onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  };
+  
+  document.getElementById("end").addEventListener("click", onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const start = { lat: lat, lng: long };
+  console.log(start);
+  const end = { lat: 43.35504, lng: -5.851287 };
+  directionsService.route(
+    {
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode.DRIVING,
+    },
+    (response, status) => {
+      if (status === "OK") {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
+}
+function mostrar(datos) {
+  lat = datos.coords.latitude;
+  long = datos.coords.longitude;
+  console.log(lat);
+  console.log(long);
+}
+var lat;
+var long;
