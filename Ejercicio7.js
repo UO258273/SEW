bd04646574431efdd1d49ce507359d5b7a505819
UO258273 <71766164+UO258273@users.noreply.github.com>
2020-11-19 17:42:54 +0100

    ds

---
+++ Ejercicio7.js
@@ -0,0 +1,90 @@
+"use restrictive";
+class Funcionalidad {
+  constructor() {
+	this.flag = false;
+  }
+
+  ocultar() {
+    $("h1").hide();
+  }
+  mostrar() {
+    $("h1").show();
+  }
+
+  modificar() {
+    debugger;
+    if (mensajeOriginal === "") {
+      mensajeOriginal = $("h2").text();
+    }
+    if (cont < mensajeOriginal.length) {
+      $("h2").text(
+        `${mensajeOriginal.substring(
+          cont,
+          mensajeOriginal.length
+        )},veces modificado: ${cont}`
+      );
+    } else {
+      $("h2").text(
+        "ya no se puede seguir modificando, veces modificado: " +
+          mensajeOriginal.length
+      );
+    }
+    cont++;
+  }
+
+  añadir() {
+    debugger;
+    $("h3").text(`${$("h3").text()} unas palabras mas añadidas`);
+  }
+
+  eliminar() {
+	$("#ph3").remove();
+	$("#eliminar").remove();
+  }
+  mostrarPadreYTipo(){
+	if(!this.flag){
+		$("*", document.body).each(function(){
+			var padre = $(this).parent().get(0);
+			var tipoPadre = padre.tagName;
+			$(this).prepend("<p>Etiqueta de su posicion actual: " + $(this).prop("tagName") + "</p>");
+			$(this).prepend("<p>Etiqueta de su padre: " + tipoPadre + "</p>");
+		});
+
+		this.flag = true;
+	}
+}
+
+sumatorioTablas(){
+	var filas = [];
+	var columnas = [];
+	var count = 0;
+
+	$("table tr").each(function(){
+		var filas = $(this).children();
+		var resultado = 0;
+
+		for(var i=0;i<filas.length;i++){
+			var columna = filas[i];
+			resultado += parseInt(columna.innerText);
+			if(count != 0)
+				columnas[i] += parseInt(columna.innerText);
+			else
+				columnas[i] = parseInt(columna.innerText);
+		}
+		
+		filas.push(resultado);
+		count++;
+	}); 
+
+	for(var i=0;i<filas.length;i++){
+		$("#tabla").append("<p>Fila: " + i + " = " + filas[i]);
+	}
+
+	for(var i=0;i<columnas.length;i++){
+		$("#tabla").append("<p>Columna: " + i + " = " + columnas[i]);
+	}
+}
+}
+var func = new Funcionalidad();
+var cont = 1;
+var mensajeOriginal = "";
