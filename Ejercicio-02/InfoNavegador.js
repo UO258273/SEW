01c63aebe82e9a00428a906a80d2941f019214c4
UO258273 <UO258273@uniovi.es>
2021-04-13 18:23:14 +0200

    limpieza

--- Ejercicio-02/InfoNavegador.js
+++
@@ -1,8 +0,0 @@
-    infoNavegador = new Object();
-    infoNavegador.nombre = navigator.appName;
-    infoNavegador.version = navigator.appVersion;
-    infoNavegador.idioma = navigator.language;
-    infoNavegador.javaActivo = navigator.javaEnabled();
-    infoNavegador.plataforma = navigator.platform;
-    infoNavegador.vendedor = navigator.vendor;
-    infoNavegador.agente = navigator.userAgent;
